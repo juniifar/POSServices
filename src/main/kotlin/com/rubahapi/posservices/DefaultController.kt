85b148c15a9e1b33ab4d54b1bf4c3cf7d592e3de
Junifar Hidayat <junifar.hidayat@dhanapala.id>
2021-06-07 22:57:59 +0700

    akldnaskljd

--- src/main/kotlin/com/rubahapi/posservices/DefaultController.kt
+++ src/main/kotlin/com/rubahapi/posservices/DefaultController.kt
@@ -1,10 +1,13 @@
 package com.rubahapi.posservices
 
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class HelloController {
+@CrossOrigin
+class DefaultController {
+
     @GetMapping("/")
     fun helloKotlin(): String{
         return "Hello World"
---
+++ src/main/kotlin/com/rubahapi/posservices/greeting/Greeting.kt
@@ -0,0 +1,3 @@
+package com.rubahapi.posservices.greeting
+
+class Greeting(val id: Long, val content: String)
