65e7420dcc92a8769a7bd51718906633066639df
Junifar Hidayat <junifar.hidayat@dhanapala.id>
2021-06-08 09:57:49 +0700

    akldnaskljd

---
+++ src/main/kotlin/com/rubahapi/posservices/model/User.kt
@@ -0,0 +1,27 @@
+package com.rubahapi.posservices.model
+
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.LastModifiedDate
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
+import java.time.LocalDateTime
+import javax.persistence.*
+
+@Entity
+@Table(name = "users")
+@EntityListeners(AuditingEntityListener::class)
+class User(
+    @Id
+    val id: Long,
+    val name: String,
+    val username: String,
+    val password: String,
+    val state: Boolean,
+    @CreatedDate
+    @Column(name = "created_date", nullable = false, updatable = false)
+    var createdDate: LocalDateTime? = null,
+    @LastModifiedDate
+    @Column(name = "last_modified_date", nullable = false)
+    var lastModifiedDate: LocalDateTime? = null
+) {
+    constructor(): this(0, "", "", "", false)
+}
